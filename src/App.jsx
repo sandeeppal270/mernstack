c3f6991362add3129b3bccec9ce7824948587e26
sandeeppal270 <64672009+sandeeppal270@users.noreply.github.com>
2022-10-20 18:01:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <center>
+          Welcome to React
+        </center>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
