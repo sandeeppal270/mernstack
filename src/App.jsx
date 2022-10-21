bd774b4c3c26fb467835112765034888d8a6fb68
sandeeppal270 <64672009+sandeeppal270@users.noreply.github.com>
2022-10-21 12:49:31 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css';
+import {useState,useEffect} from "react";
+
+// functions
+
+import { getTest } from "./functions/test";
+
+
 
 function App() {
+  const [data,setData] = useState("Welcome to React");
+
+useEffect(()=>{
+getTest()
+.then((res)=>{
+  setData(res.message);
+})
+.catch((err)=>console.log(err));
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
         <center>
-          Welcome to React
+        <h1>{data}</h1>
         </center>
         
       </header>
